8409a89298d97f79074f8eeaee02a7d18d41001a
Shreeshaa S <shreeshaa2007@gmail.com>
2025-12-11 07:13:21 +0000

    create a app which has a very simple user login and the doctors login. i

--- .next/static/chunks/src_app_layout_tsx_0a548d63._.js
+++ .next/static/chunks/src_app_layout_tsx_0a548d63._.js
@@ -1,9 +1,9 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/[root-of-the-server]__f6202ce6._.css",
+  "static/chunks/[root-of-the-server]__e0583ea8._.css",
   "static/chunks/node_modules_b3f3d213._.js",
-  "static/chunks/src_8f79f3e9._.js"
+  "static/chunks/src_2bc135c0._.js"
 ],
     source: "dynamic"
 });
